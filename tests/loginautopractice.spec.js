5a2bda9c8578a94e70db685fca9f0dee7b30be27
G Thimmaraju <thimmaraju418@gmail.com>
2025-12-23 10:07:12 +0530

    env files

---
+++ tests/loginautopractice.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test("Verify Login to Automation Practice site ", async ({page}) => {
+
+    await page.goto('https://automationexercise.com/login')
+
+    await page.locator('//input[@data-qa="login-email"]').fill(process.env.APP_USERNAME)
+
+    await page.locator('//input[@data-qa="login-password"]').fill(process.env.APP_PASSWORD)
+
+    await page.locator('//button[@data-qa="login-button"]').click()
+    
+    await expect(page.locator('//a[@href="/logout"]')).toBeVisible()
+
+})
